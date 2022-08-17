fd35800f68234416457ddef786ab5d40c00e473f
Julia Prado <99263001+juliapradob@users.noreply.github.com>
2022-08-16 21:10:23 -0300

    refactor: cria componente do formulário e retira do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Menu from './components/Menu'
+import Form from './components/Form'
 
 function App() {
   return (
@@ -13,25 +14,7 @@ function App() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies tellus nec mi porta convallis sollicitudin eu urna. Mauris rhoncus erat sed interdum dignissim. Suspendisse semper pretium consectetur. Praesent bibendum arcu risus, sit amet iaculis ex tempus quis. Cras et erat ut tellus vulputate tincidunt. Aenean lacinia euismod augue vel egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum vel urna tortor. Vivamus et arcu non felis tristique eleifend. <br></br><br></br>Morbi eu condimentum urna. Curabitur eu magna eget turpis condimentum ultrices. Suspendisse quis lorem ultricies, pulvinar purus sed, egestas erat. Etiam ultricies a ante vehicula pharetra. Quisque ut neque mattis, consequat velit ut, ultrices orci. Nulla varius elementum erat vel pharetra. Aenean sit amet nisi diam. Morbi viverra, magna ac luctus commodo, odio ante suscipit libero, at mattis augue est vel metus.</p>
         </div>
         <div className='heroLeft'>
-          <form>
-            <label>Seu nome:</label>
-            <input className='campo'></input>
-            <label>E-mail:</label>
-            <input className='campo'></input>
-            <label>CPF:</label>
-            <input className='campo'></input>
-            <div className='heroLeftSexo'>
-              <div>
-                <input type='radio' value='Masculino' id='masculino' name='sexo'></input>
-                <label for='masculino'>Masculino</label>
-              </div>
-              <div>
-                <input type='radio' value='Feminino' id='feminino' name='sexo'></input>
-                <label for='feminino'>Feminino</label> 
-              </div>
-            </div>
-            <button>Enviar</button>
-          </form>
+          <Form />
         </div>
       </div>
     </div>
---
+++ src/components/Form.jsx
@@ -0,0 +1,23 @@
+const Form = () => {
+    return (<form>
+        <label>Seu nome:</label>
+        <input className='campo'></input>
+        <label>E-mail:</label>
+        <input className='campo'></input>
+        <label>CPF:</label>
+        <input className='campo'></input>
+        <div className='heroLeftSexo'>
+            <div>
+                <input type='radio' value='Masculino' id='masculino' name='sexo'></input>
+                <label for='masculino'>Masculino</label>
+            </div>
+            <div>
+                <input type='radio' value='Feminino' id='feminino' name='sexo'></input>
+                <label for='feminino'>Feminino</label> 
+            </div>
+        </div>
+        <button>Enviar</button>
+        </form>)
+}
+
+export default Form
